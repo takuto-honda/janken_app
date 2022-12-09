fcffd4429ec8492916322a2228d180c7950a9723
honda_takuto <takuto.engineer@gmail.com>
2022-12-09 17:31:05 +0900

    HTML作成

--- janken.php
+++ janken.php
@@ -7,6 +7,17 @@
   <title>Document</title>
 </head>
 <body>
-  
+<p>じゃんけん！...
+  <select>
+    <option>グー</option>
+    <option>チョキ</option>
+    <option>パー</option>
+  </select>
+</p>
+
+<p>おめでとう！君の勝利だ！！</p>
+<p>あいこ！次はどうする？</p>
+<p>残念...でも諦めるな！！</p>
+
 </body>
 </html>
