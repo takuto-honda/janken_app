52296c7cda44afab1ef293a62a6dbb95e6f41caa
honda_takuto <takuto.engineer@gmail.com>
2022-12-10 12:39:42 +0900

    フォーム追加

--- janken.php
+++ janken.php
@@ -8,20 +8,23 @@
   <link rel="stylesheet" href="./app.css">
 </head>
 <body>
-<div class="preparation">じゃんけん！...</div>
-<div class="box">
-  <select>
-    <option value="rock">グー</option>
-    <option value="scissors">チョキ</option>
-    <option value="paper">パー</option>
-  </select>
-</div>
-
+<div class="preparation">いざ尋常にじゃんけん！...</div>
+<br>
+<br>
+<form action="janken.php" method="POST">
+  <div class="box">
+    <select name="action">
+      <option value="rock" name="rock">グー</option>
+      <option value="scissors" name="scissors">チョキ</option>
+      <option value="paper" name="paper">パー</option>
+    </select>
+  </div>
+  <button class="action-button" type="submit">決定</button>
+</form>
 <div class="result">
   <div class="win">おめでとう！<br>君の勝利だ！！</div>
   <div class="draw">あいこ！<br>次はどうする？</div>
   <div class="lose">残念...<br>また頑張ろう！！</div>
 </div>
-
 </body>
 </html>
